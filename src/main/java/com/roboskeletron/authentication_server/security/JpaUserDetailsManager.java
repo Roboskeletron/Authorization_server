e3a75f9b4bbd6ae6ab559d4ccd51f5aca5dc8cab
Roboskeletron <wwwrt287@gmail.com>
2023-06-20 22:43:05 +0300

    createUser fixed, updateUser and changePassword are deprecated, replacements added

--- src/main/java/com/roboskeletron/authentication_server/security/JpaUserDetailsManager.java
+++ src/main/java/com/roboskeletron/authentication_server/security/JpaUserDetailsManager.java
@@ -1,11 +1,14 @@
 package com.roboskeletron.authentication_server.security;
 
-import com.roboskeletron.authentication_server.service.UserScopeService;
+import com.roboskeletron.authentication_server.domain.User;
 import com.roboskeletron.authentication_server.service.UserService;
 import com.roboskeletron.authentication_server.util.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,16 +16,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class JpaUserDetailsManager implements UserDetailsManager {
     private final UserService userService;
-    private final UserScopeService userScopeService;
 
     @Override
     public void createUser(UserDetails user) {
-        userService.createUser(UserMapper.fromUserDetails(user, userScopeService));
+        userService.createUser(UserMapper.fromUserDetails(user));
     }
 
     @Override
+    @Deprecated
     public void updateUser(UserDetails user) {
-        userService.updateUser(UserMapper.fromUserDetails(user, userScopeService));
+        userService.updateUser(UserMapper.fromUserDetails(user));
+    }
+
+    public void updateUser(User user){
+        userService.updateUser(user);
+    }
+
+    public void changePassword(User user, String oldPassword, String newPassword){
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        if (!passwordEncoder.matches(oldPassword, user.getPassword()))
+            throw new BadCredentialsException("Invalid password");
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+
+        updateUser(user);
     }
 
     @Override
@@ -31,6 +48,7 @@ public class JpaUserDetailsManager implements UserDetailsManager {
     }
 
     @Override
+    @Deprecated
     public void changePassword(String oldPassword, String newPassword) {
 
     }
