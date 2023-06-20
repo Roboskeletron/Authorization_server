8e02ff3239716c637f38a584b7cb8a5961f10c07
Roboskeletron <wwwrt287@gmail.com>
2023-06-20 22:40:38 +0300

    map scopes by name

--- src/main/java/com/roboskeletron/authentication_server/util/UserMapper.java
+++ src/main/java/com/roboskeletron/authentication_server/util/UserMapper.java
@@ -1,19 +1,19 @@
 package com.roboskeletron.authentication_server.util;
 
 import com.roboskeletron.authentication_server.domain.User;
-import com.roboskeletron.authentication_server.service.UserScopeService;
-import org.springframework.security.core.GrantedAuthority;
+import com.roboskeletron.authentication_server.domain.UserScope;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.stream.Collectors;
 
 public class UserMapper {
-    public static User fromUserDetails(UserDetails userDetails, UserScopeService service){
+    public static User fromUserDetails(UserDetails userDetails){
         return User.builder()
                 .username(userDetails.getUsername())
                 .password(userDetails.getPassword())
-                .scopes(userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-                        .map(service::getUserScope).collect(Collectors.toSet()))
+                .scopes(userDetails.getAuthorities().stream()
+                        .map(scope -> UserScope.builder().name(scope.getAuthority()).build())
+                        .collect(Collectors.toSet()))
                 .build();
     }
 }
